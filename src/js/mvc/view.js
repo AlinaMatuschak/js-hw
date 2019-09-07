aab84fc1dd5c81342ae6672a36b85a862e3703a4
Alina <alina90565@gmail.com>
2019-09-07 14:25:10 +0300

    delete consol in view

--- src/js/mvc/view.js
+++ src/js/mvc/view.js
@@ -2,7 +2,7 @@ import { refs } from "../utils/constants";
 import template from "../templates/note.hbs";
 
 const array = JSON.parse(localStorage.getItem('notes'));
-console.log(template);
+
 const refreshList = data => {
   const notes = data || array;
   const htmlNotesList = template({ notes });
